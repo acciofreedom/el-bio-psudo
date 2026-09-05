e7fe87d37bdac0b6326a0dd115fca8a09de99179
acciofreedom <acciofreedom@outlook.kr>
2026-09-06 08:51:08 +0900

    test: strengthen jest config (moduleDirectories, transform) for CI module resolution

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,1 +1,15 @@
-﻿module.exports = { preset: 'ts-jest', testEnvironment: 'jsdom', moduleNameMapper: { '\\.(css|less|scss|sass)$': 'identity-obj-proxy' }, setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'] };
+﻿module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
+};
