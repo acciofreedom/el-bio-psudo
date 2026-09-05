38424edbd997d5c08b2f56ddac9ebfaa96a7b1e9
acciofreedom <acciofreedom@outlook.kr>
2026-09-06 08:49:05 +0900

    test: explicitly import and extend @testing-library/jest-dom matchers for CI

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,6 @@
-﻿import '@testing-library/jest-dom/extend-expect';
+// Explicitly load matchers in CI-friendly way
+import '@testing-library/jest-dom';
+import matchers from '@testing-library/jest-dom/matchers';
 
+// Extend Jest expect with Testing Library matchers
+(expect as any).extend(matchers);
